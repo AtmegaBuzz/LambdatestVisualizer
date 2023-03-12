9b35d4bdb4b14b8bb63cfc69ae349c05253c6ae8
AtmegaBuzz <swapnilshinde9382@gmail.com>
2023-03-13 01:58:34 +0530

    elasticsearch support for TestEngine scrapper

--- scripts/scrapper.py
+++ scripts/scrapper.py
@@ -1,77 +1,121 @@
-from selenium import webdriver
-import chromedriver_autoinstaller
 import json
+import chromedriver_autoinstaller
 
-from selenium.webdriver.common.keys import Keys
+from uuid import uuid4
+from elasticsearch import Elasticsearch
+
+from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-from time import sleep
 
-chromedriver_autoinstaller.install()
+class TestEngine:
 
-LOADING_SLEEP= 10
+    def __init__(
+            self,
+            initial_site_routes,
+            chrome_options,
+            capabilities,
+            loading_sleep,
+            es_url="http://localhost:9200",
+            es_index="network_logs"
+            ):
 
-
-def process_browser_log_entry(entry):
-    response = json.loads(entry['message'])['message']
-    return response
-
-options = Options()
-options.add_argument("--start-maximized")
-
-
-caps = DesiredCapabilities.CHROME
-caps['goog:loggingPrefs'] = {'performance': 'ALL'}
-driver = webdriver.Chrome(options=options,desired_capabilities=caps)
-driver.implicitly_wait(0.5)
-driver.get("https://www.lambdatest.com/")
-
-
-# navbar links
-site_routes = [
-    "https://www.lambdatest.com/",
-    "https://www.lambdatest.com/enterprise",
-    "https://www.lambdatest.com/pricing"
-]
-
-for a_tags in driver.find_elements(By.CLASS_NAME,"header_inner"):
+        self.es = Elasticsearch(es_url)
+        self.es_index = es_index
+        self.LOADING_SLEEP = loading_sleep
+        self.options = chrome_options
+        self.caps = capabilities
+        self.site_routes = initial_site_routes
     
-    for href in a_tags.find_elements(By.TAG_NAME,"a"):
-        site_routes.append(href.get_attribute("href"))
+
+    def elastic_insert(self,body):
+        self.es.index(
+            index=self.es_index,
+            id=uuid4(),
+            body=body
+        )
+
+    @staticmethod
+    def process_browser_log_entry(entry):
+        response = json.loads(entry['message'])['message']
+        return response
 
 
-for link in site_routes:
-
-    driver.get(link)
-    # wait till the site is loaded
-    WebDriverWait(driver,LOADING_SLEEP).until(EC.presence_of_element_located((By.TAG_NAME,"body")))
-
-
-    browser_log = driver.get_log('performance') 
-    events = [process_browser_log_entry(entry) for entry in browser_log]
-    events = [event for event in events if 'Network.response' in event['method']]
-
-    for packet in events:
-
-        if "response" not in packet["params"]:
-            continue
-
-        print(packet["params"]["response"]["mimeType"])
-        packet["params"]["response"]["status"]
-        packet["params"]["response"]["url"]
-        packet["params"]["response"]["securityState"]
-        packet["params"]["response"]["responseTime"]
-        packet["params"]["response"]["protocol"]
-        packet["params"]["type"]
+    def run(self):
         
-        
+        driver = webdriver.Chrome(
+            options=self.options,
+            desired_capabilities=self.caps
+        )
+
+        driver.implicitly_wait(0.5)
+
+        driver.get(self.site_routes[0])
+
+        for a_tags in driver.find_elements(By.CLASS_NAME,"header_inner"):
+    
+            for href in a_tags.find_elements(By.TAG_NAME,"a"):
+                site_routes.append(href.get_attribute("href"))
+
+
+        for link in site_routes:
+
+            driver.get(link)
+
+            # wait till the site is loaded
+            WebDriverWait(driver,self.LOADING_SLEEP).until(EC.presence_of_element_located((By.TAG_NAME,"body")))
+
+
+            browser_log = driver.get_log('performance') 
+            events = [TestEngine.process_browser_log_entry(entry) for entry in browser_log]
+            events = [event for event in events if 'Network.response' in event['method']]
+
+            for packet in events:
+
+                if "response" not in packet["params"]:
+                    continue
+
+                self.elastic_insert(packet)
+                
+
+        driver.quit()
 
 
 
 
-driver.quit()
+if __name__ == "__main__":
+
+
+    chromedriver_autoinstaller.install()
+
+    LOADING_SLEEP= 10
+
+
+    options = Options()
+    options.add_argument("--start-maximized")
+
+    caps = DesiredCapabilities.CHROME
+    caps['goog:loggingPrefs'] = {'performance': 'ALL'}
+
+
+
+    # navbar links
+    site_routes = [
+        "https://www.lambdatest.com/",
+        "https://www.lambdatest.com/enterprise",
+        "https://www.lambdatest.com/pricing"
+    ]
+
+
+    network_test = TestEngine(
+        site_routes,
+        options,
+        caps,
+        20
+    )
+
+    network_test.run()
