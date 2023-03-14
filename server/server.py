f345e681fb341c985765e4d82f71a3246df7e7bf
AtmegaBuzz <swapnilshinde9382@gmail.com>
2023-03-15 01:41:28 +0530

    added status code logs api

--- server/server.py
+++ server/server.py
@@ -1,12 +1,28 @@
 import datetime
+import json
 from elasticsearch import Elasticsearch
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"], # Allows all methods
+    allow_headers=["*"]
+)
 es = Elasticsearch("http://localhost:9200")
+
+class MimeTypePost(BaseModel):
+    mimeType:str
+
+class StatusCodePost(BaseModel):
+    statusCode:int
+
 
 
 @app.get("/")
@@ -34,11 +50,63 @@ def home():
 
         yearly_log_count.append(res["count"])
 
-    return {"count":yearly_log_count}
+    return yearly_log_count
 
 
 @app.get("/mimeType")
-def filter():
+def getMimeType():
+
+    mime_typs = []
+
+    res = es.search(
+            index="network_logs",
+
+            body={
+                "_source":[],
+                "size":0,
+                "aggs":{
+                    "by_mimeType":{
+                        "terms":{
+                            "field":f"params.response.mimeType.keyword",
+                        }
+                    }
+                }
+            }
+        )
+    
+    mime_typs = [mimeType["key"] for mimeType in res["aggregations"]["by_mimeType"]["buckets"]]
+    return mime_typs
+
+
+
+@app.get("/mimeTypesLog")
+def mimeTypesLog():
+
+    res = es.search(
+                index="network_logs",
+
+                body={
+                    "_source":[],
+                    "size":0,
+                    "aggs":{
+                        "by_mimeType":{
+                            "terms":{
+                                "field":f"params.response.mimeType.keyword",
+                                "order":{
+                                    "_count":"desc"
+                                },
+                            }
+                        }
+                    }
+                }
+            )
+   
+    return res["aggregations"]["by_mimeType"]["buckets"]
+
+
+
+@app.post("/mimeType")
+def postMimeType(mimeType:MimeTypePost):
 
     yearly_log_count = []
 
@@ -54,9 +122,13 @@ def filter():
                     "_source":[],
                     "size":0,
                     "query":{
-                        "match":{
-                                "datetime":date,
-                        },
+                        "bool":{
+                            "must":[
+                                    {"match" : { "datetime":date, }},
+                                    {"match" : { "params.response.mimeType.keyword":mimeType.mimeType}}
+                            ]
+                        }  
+                        
                     },
                     "aggs":{
                         "by_mimeType":{
@@ -70,12 +142,43 @@ def filter():
                     }
                 }
             )
-        yearly_log_count.append(res["aggregations"]["by_mimeType"]["buckets"])
+
+        if  len(res["aggregations"]["by_mimeType"]["buckets"])!=0:
+            yearly_log_count.append((res["aggregations"]["by_mimeType"]["buckets"][0]["doc_count"]))
+        else:
+            yearly_log_count.append(0)
 
     return {"count":yearly_log_count}
 
+
 @app.get("/statusCode")
-def filter():
+def getStatusCode():
+
+    mime_typs = []
+
+    res = es.search(
+            index="network_logs",
+
+            body={
+                "_source":[],
+                "size":0,
+                "aggs":{
+                    "by_statusCode":{
+                        "terms":{
+                            "field":f"params.response.status",
+                        }
+                    }
+                }
+            }
+        )
+    
+    mime_typs = [mimeType["key"] for mimeType in res["aggregations"]["by_statusCode"]["buckets"]]
+    return mime_typs
+
+
+
+@app.post("/statusCode")
+def postStatusCode(statusCode:StatusCodePost):
 
     yearly_log_count = []
 
@@ -91,9 +194,13 @@ def filter():
                     "_source":[],
                     "size":0,
                     "query":{
-                        "match":{
-                                "datetime":date,
-                        },
+                        "bool":{
+                            "must":[
+                                    {"match" : { "datetime":date, }},
+                                    {"match" : { "params.response.status":statusCode.statusCode}}
+                            ]
+                        }  
+                        
                     },
                     "aggs":{
                         "by_statusCode":{
@@ -107,7 +214,37 @@ def filter():
                     }
                 }
             )
-        yearly_log_count.append(res["aggregations"]["by_statusCode"]["buckets"])
-    print(len(yearly_log_count))
+
+        if  len(res["aggregations"]["by_statusCode"]["buckets"])!=0:
+            yearly_log_count.append((res["aggregations"]["by_statusCode"]["buckets"][0]["doc_count"]))
+        else:
+            yearly_log_count.append(0)
+
     return {"count":yearly_log_count}
+
+
+
+@app.get("/statusCodeLogs")
+def statusCodeLogs():
+
+    res = es.search(
+                index="network_logs",
+
+                body={
+                    "_source":[],
+                    "size":0,
+                    "aggs":{
+                        "by_statusCode":{
+                            "terms":{
+                                "field":f"params.response.status",
+                                "order":{
+                                    "_count":"desc"
+                                },
+                            }
+                        }
+                    }
+                }
+            )
+   
+    return res["aggregations"]["by_statusCode"]["buckets"]
 
